d610a8df6d75e8f55426ab2ae967fae56030160a
Utshab Kumar Ghosh <kumarutshab@gmail.com>
2020-10-15 03:41:35 +0600

    downloading from drive added

--- weightlift/modeling/urls.py
+++ weightlift/modeling/urls.py
@@ -1,7 +1,8 @@
 # CONFIGURE API URL
 from django.urls import path
-from .views import weight
+from . import views
 
 urlpatterns = [
-    path('api/predict/', weight, name='weight'),
+    path("", views.home, name='home'),
+    path('video', views.video, name='video')
 ]
--- weightlift/weightlift/urls.py
+++ weightlift/weightlift/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('model/', include('modeling.urls'))
+    path('', include('modeling.urls')),
 ]
